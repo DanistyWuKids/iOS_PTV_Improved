4ceb9678b8d13893a9450c5488c9f726d7561c56
DanistyWuKids <wgong0001@gmail.com>
2019-10-20 22:59:28 +1100

    Table Baked

---
+++ config/schema/script.sql
@@ -0,0 +1,16 @@
+DROP TABLE "Users";
+DROP TABLE "Settings";
+
+CREATE TABLE "Users" (
+"id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+"username" TEXT NOT NULL,
+"password" TEXT NOT NULL
+);
+
+CREATE TABLE "Settings" (
+"id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+"attr_text" TEXT,
+"attr_int" INTEGER
+);
+
+
